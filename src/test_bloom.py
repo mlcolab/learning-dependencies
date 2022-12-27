8933d7e97d6d5674f254411be2f033749a31a40e
Dominik Glandorf <do.gl@gmx.de>
2022-12-27 16:53:58 +0100

    add LD experiments

---
+++ src/test_bloom.py
@@ -0,0 +1,9 @@
+from transformers import AutoTokenizer,AutoModelForCausalLM
+
+model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-1b7")
+tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-1b7", device_map="auto")
+
+text_prompt ="What should I know to understand singular value decomposition?"
+inputs = tokenizer(text_prompt, return_tensors="pt").input_ids.to("cuda")
+result = model.generate(inputs,max_length=200,top_k=0,temperature=0.5)
+print(tokenizer.decode(result[0], truncate_before_pattern=[r"\n\n^#","^'''","\n\n\n"]))

fc1f345b06ab7f03f7cb39cae67e4c8eb58d7960
Dominik Glandorf <do.gl@gmx.de>
2023-01-24 11:05:54 +0100

    threshold metrics and validation

--- src/data_layer.py
+++ src/data_layer.py
@@ -2,23 +2,27 @@ import os
 import pandas as pd
 import numpy as np
 import pickle
+from collections import Counter
 
 def get_smallest_page(pages):
     if not isinstance(pages, float) and "nan" in pages: return None
     pages_int = [int(pages)] if isinstance(pages, float) else list(map(int, pages.split(",")))
     return min(pages_int)
 
-def read_index_and_wiki_concepts():
+def read_index_and_wiki_concepts(include_counts=False):
     path = "../dat/index_by_wiki/"
-    wiki_concepts = set()
+    wiki_concepts = []
     indices = {}
+    
     for book in os.listdir(path):
         index = pd.read_csv(path + book).dropna()
         index['first_page'] = index.pages.apply(get_smallest_page) # add first page of index entry
         indices[book] = index
-        wiki_concepts.update(index.wiki_concept) # add new concepts to set
+        wiki_concepts.extend(index.wiki_concept) # add new concepts to set
+    counts = Counter(wiki_concepts)
+    wiki_concepts = counts.keys()
     wiki_concepts = np.array(list(wiki_concepts))
-    
+    if include_counts: return indices, wiki_concepts, pd.DataFrame({"concept": wiki_concepts, "count": counts.values()})
     return indices, wiki_concepts
 
 def get_mentions_by_offset(book, min_pr=0.001, min_span=4):
